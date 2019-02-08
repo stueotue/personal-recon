39eae31ac694573611a4fe46dc3985af89671ace
richardtelleng <stueotue@gmail.com>
2019-02-09 06:59:58 +0800

    Add files via upload

---
+++ Re-con.bat
@@ -0,0 +1,33 @@
+@echo off
+echo       ############################
+echo .                  Hi 		            . 
+echo .                                      . 
+echo .            How you doin ?            . 
+echo       ############################
+echo ----------------------------------------
+set year=%date:~-4%
+set month=%date:~4,2%
+set day=%date:~7,2%
+set rootpath=R:\Richard\lab\Recon\result\
+set aquatonepath=R:\Richard\Files\Tools\aquatone\
+set amasspath=R:\Richard\lab\Tools\amass\
+set dirsearchpath=R:\Richard\Files\Tools\dirsearch\
+set chromiumpath=R:\Richard\Files\Tools\aquatone\chromium\bin\chrome.exe
+set /p domain="Domain to enumerate : "
+cls
+echo Creating Folder for %domain% 
+set saveresult=%rootpath%%domain%-%year%-%month%-%day%
+mkdir %saveresult%
+echo Enumerate %domain%
+echo Amass is running...
+%amasspath%amass -d %domain% -o %saveresult%\%domain%.txt --passive
+echo Amass Done
+echo Aquatone Enumerate %domain%
+echo Starting Aquatone...
+type "%saveresult%\%domain%.txt" | %aquatonepath%aquatone -out %saveresult%\aquatone-report\ -chrome-path %chromiumpath%
+echo Aquatone Done
+echo Performing Dirsearch...
+python3 %dirsearchpath%dirsearch.py -L %saveresult%\aquatone-report\aquatone_urls.txt -e *
+echo Perform cc.py
+python3 R:\Richard\lab\Tools\cc.py\cc.py -o %saveresult%\cc-%domain%.txt -u %domain%
+echo Done ! Save every result in %saveresult%
